3dc90011a59c5f4d2868abededb3241e9bf9e07a
Bryson Shields <brysonshields430@gmail.com>
2024-03-30 09:16:04 -0700

    Day 1

--- Projects/Python/table.py
+++ Projects/Python/table.py
@@ -15,19 +15,19 @@ def test(v, h):
             print(total, end = ", ")
             hori += 1
 #write how much you want the table
-print("how many rows do you want")
+print("how many rows do you want.")
 rows = int(input())
-print("how many columns do you want")
+print("how many columns do you want.")
 columns = int(input())
-print(f"Are you sure. {rows} * {columns}")
+print(f"Are you sure. {rows} * {columns} Y/N (Yes/No) ")
 sure = input()
-while sure == "N":
-    print("how many rows do you want")
+while sure == "N" or sure == "No" or sure == "no" or sure == "n":
+    print("how many rows do you want.")
     rows = int(input())
-    print("how many columns do you want")
+    print("how many columns do you want.")
     columns = int(input())
-    print(f"Are you sure. {rows} * {columns}")
+    print(f"Are you sure. {rows} * {columns} Y/N (Yes/No)")
     sure = input()
 
-if sure == "Y":
+if sure == "Y" or sure == "Yes" or sure == "yes" or sure == "y":
     test(rows, columns)
