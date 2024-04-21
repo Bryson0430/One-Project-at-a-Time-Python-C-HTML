5da453d7bfb83cd38e39c749d4a3caea9b9c9cc6
Bryson <brysonshields430@gmail.com>
2024-04-21 13:43:06 -0700

    new javascript project

--- Projects/Javascript/test.js
+++ Projects/Javascript/test.js
@@ -1,1 +1,37 @@
+let i = [
+    "1",
+    4,
+    "4",
+    4,
+]
 
+function findsc(list, val) {
+    let numinval = 0 
+    for (let count in list) {
+        if (list[count] === val) {
+            numinval++;
+            continue;
+        }
+        else {
+            continue;
+        }
+    }
+    return numinval;
+}
+function find(list, val) {
+    let numinval = 0 
+    for (let count in list) {
+        if (list[count] == val) {
+            numinval++;
+            continue;
+        }
+        else {
+            continue;
+        }
+    }
+    return numinval;
+}
+
+value = findsc(i, 4)
+
+console.log(value)
--- Website/Javascript/testp.js
+++ Website/Javascript/testp.js
@@ -3,17 +3,49 @@
 function submit() {
 html = document.querySelector("html")
 range = document.querySelector("#range")
-text = document.querySelector("p").innerHTML
+textp = document.querySelector("#vrange").innerHTML
+texti = document.querySelector("#itext").innerHTML
 
-text = "value of " + range.id + ": " + range.value
+textp = "value of " + range.id + ": " + range.value
 
-document.querySelector("p").innerHTML = text
+document.querySelector("#vrange").innerHTML = textp
+
+range.value = ""
 }
 
 function reset() {
-    text = "value of range: None"
 
-    document.querySelector("p").innerHTML = text
+    range.value = ""
+    textp = "value of range: None"
+
+    document.querySelector("#vrange").innerHTML = textp
 
 
+}
+
+i = 1
+    
+function changet() {
+    if (i == 1) {
+        range.type = "text"
+    }
+    if (i == 2) {
+        range.type = "range"
+    }
+    if (i == 3) {
+        range.type = "file"
+    }
+    if (i == 4) {
+        range.type = "date"
+    }
+    if (i == 5) {
+        range.type = "time"
+    }
+   
+    if (i > 5) {
+        i = 0
+    }
+ i++
+    texti = "value of i: " + range.type
+    document.querySelector("#itext").innerHTML = texti
 }
